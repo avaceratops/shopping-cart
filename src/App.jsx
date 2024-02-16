a0deec87cf835fadd54670147ac760bd9ef56a3b
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-16 15:11:41 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        Shopping Cart
+      </h1>
+    </>
+  );
+}
