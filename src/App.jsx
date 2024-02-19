feca90d50f324b3b10b5e40f4584587328df4704
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-19 11:47:15 +0000

    Add route configuration using React Router
    The root route displays the header and footer, and includes two nested
    routes.
    
    First, the Products route renders products associated with a particular
    game. This is accessible via the dynamic segment ":game".
    
    Second, the Product route renders detailed information about a specific
    product. This is accessible via the dynamic segment ":game/:productId".

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-        Shopping Cart
-      </h1>
-    </>
-  );
-}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/Root.jsx';
+import Products from './routes/Products.jsx';
+import Product from './routes/Product.jsx';
 import './index.scss';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: ':game',
+        element: <Products />,
+      },
+      {
+        path: ':game/:productId',
+        element: <Product />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
