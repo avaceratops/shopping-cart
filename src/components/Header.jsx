f8673f6010f96f55b0781b58d5dc751065d40860
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-20 12:31:58 +0000

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+import CartButton from './CartButton';
+import '../styles/Header.scss';
+
+export default function Header() {
+  return (
+    <header className="px-5">
+      <div className="border-b border-gray-200">
+        <nav className="flex h-16 items-center justify-between text-gray-700">
+          <section className="flex-1">
+            <h1 className="text-2xl font-bold leading-none tracking-tight text-black">
+              shopping cart
+            </h1>
+          </section>
+          <ul className="flex h-full space-x-8 text-sm font-medium">
+            <NavLink to="/">
+              <button>Home</button>
+            </NavLink>
+            <NavLink to="/warhammer-40k">
+              <button>Warhammer 40,000</button>
+            </NavLink>
+            <NavLink to="/age-of-sigmar">
+              <button>Age of Sigmar</button>
+            </NavLink>
+          </ul>
+          <section className="flex flex-1 justify-end">
+            <CartButton />
+          </section>
+        </nav>
+      </div>
+    </header>
+  );
+}
