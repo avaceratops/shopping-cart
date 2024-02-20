da289a291ad0ea1f3e5a97672f6905471c485d1c
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-20 14:44:14 +0000

    Add 404 page

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,8 @@
+export default function Error() {
+  return (
+    <article className="grid h-72 place-content-center justify-items-center">
+      <h1 className="mb-4 text-6xl font-bold">404</h1>
+      <p className="italic text-gray-600">Page Not Found</p>
+    </article>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/Root.jsx';
-import Products from './routes/Products.jsx';
-import Product from './routes/Product.jsx';
+import Root from './routes/Root';
+import Products from './routes/Products';
+import Product from './routes/Product';
+import ErrorMessage from './components/ErrorMessage';
 import './index.scss';
 
 const router = createBrowserRouter([
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: ':game/:productId',
         element: <Product />,
+      },
+      {
+        path: '/*',
+        element: <ErrorMessage />,
       },
     ],
   },
