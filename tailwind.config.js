7b4ab5fc7bad4c06a38fde619ff990a1282d8736
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-20 17:57:42 +0000

    Add custom Tailwind breakpoint at 420px

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          '"Inter var", sans-serif',
-          {
-            fontFeatureSettings: '"cv11", "ss01"',
-            fontVariationSettings: '"opsz" 32',
-          },
-        ],
-      },
+    fontFamily: {
+      sans: [
+        '"Inter var", sans-serif',
+        {
+          fontFeatureSettings: '"cv11", "ss01"',
+          fontVariationSettings: '"opsz" 32',
+        },
+      ],
+    },
+    screens: {
+      xs: '420px',
+      ...defaultTheme.screens,
     },
   },
   plugins: [],
