4abfa1b32f6d338a7f51c977079c99f18aa3add7
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-21 23:31:26 +0000

    Add additional sort functions to util/products.js
    Added:
    - sortByBestSelling
    - sortByNameAsc
    - sortByNameDes
    - sortByPriceAsc
    - sortByPriceDes

--- src/utils/products.js
+++ src/utils/products.js
@@ -8,3 +8,23 @@ export function sortByAvailability(a, b) {
   }
   return 0;
 }
+
+export function sortByBestSelling(a, b) {
+  return b.sold - a.sold;
+}
+
+export function sortByNameAsc(a, b) {
+  return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
+}
+
+export function sortByNameDes(a, b) {
+  return b.name > a.name ? 1 : a.name > b.name ? -1 : 0;
+}
+
+export function sortByPriceAsc(a, b) {
+  return a.price - b.price;
+}
+
+export function sortByPriceDes(a, b) {
+  return b.price - a.price;
+}
