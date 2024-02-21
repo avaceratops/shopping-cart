0ff8d5fad6817f33d1760095642965d4392c1842
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-21 18:15:10 +0000

    Make Header navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,33 @@
 import { NavLink } from 'react-router-dom';
+import { NAVIGATION_LINKS } from '../utils/navigation';
 import CartDialog from './CartDialog';
-import '../styles/Header.scss';
+import Hamburger from './Hamburger';
+import styles from '../styles/NavLink.module.scss';
 
 export default function Header({ cart, updateCartItem, removeFromCart, clearCart }) {
   return (
     <header className="px-5">
       <div className="border-b border-gray-200">
         <nav className="flex h-16 items-center justify-between text-gray-700">
-          <section className="flex-1">
+          <section className="flex flex-1 md:hidden">
+            <Hamburger />
+          </section>
+
+          <section className="hidden xs:block lg:flex-1">
             <h1 className="text-2xl font-bold leading-none tracking-tight text-black">
               shopping cart
             </h1>
           </section>
 
-          <ul className="flex h-full space-x-8 text-sm font-medium">
-            <NavLink to="/">
-              <button>Home</button>
-            </NavLink>
-            <NavLink to="/warhammer-40k">
-              <button>Warhammer 40,000</button>
-            </NavLink>
-            <NavLink to="/age-of-sigmar">
-              <button>Age of Sigmar</button>
-            </NavLink>
+          <ul className="hidden h-full space-x-8 text-sm font-medium md:flex">
+            {NAVIGATION_LINKS.map((item) => (
+              <NavLink key={item.name} to={item.to} className={styles.navLink}>
+                {item.name}
+              </NavLink>
+            ))}
           </ul>
 
-          <section className="flex flex-1 justify-end">
+          <section className="flex flex-1 justify-end md:flex-initial lg:flex-1">
             <CartDialog
               cart={cart}
               updateCartItem={updateCartItem}
