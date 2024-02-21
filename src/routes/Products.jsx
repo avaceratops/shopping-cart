105bd5e5925daa769276ca1604c61a47cba77a13
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-21 23:53:48 +0000

    Add 'Sort By' select box to Products page
    Products can now be sorted by:
    - Best selling
    - Alphabetically, A-Z
    - Alphabetically, Z-A
    - Price ascending
    - Price descending

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react';
 import { useOutletContext, useParams } from 'react-router-dom';
-import { sortByAvailability } from '../utils/products';
+import { getSortFunction, sortByAvailability } from '../utils/products';
 import ErrorMessage from '../components/ErrorMessage';
 import LoadingSpinner from '../components/LoadingSpinner';
 import ProductCard from '../components/ProductCard';
+import styles from '../styles/Select.module.scss';
 
 export default function Products() {
   const { game } = useParams();
   const { data, isLoading } = useOutletContext();
+  const [sortMethod, setSortMethod] = useState('best-selling');
   const gameProducts = data[game];
-  const sortedProducts = gameProducts?.sort(sortByAvailability);
 
   if (isLoading) {
     return <LoadingSpinner />;
@@ -18,10 +20,37 @@ export default function Products() {
     return <ErrorMessage />;
   }
 
+  const handleChange = (e) => {
+    setSortMethod(e.target.value);
+  };
+
+  const sortFunction = getSortFunction(sortMethod);
+  // always move out-of-stock products to the end
+  const sortedProducts = [...gameProducts].sort(sortFunction).sort(sortByAvailability);
+
   return (
     <>
+      <section className="mb-4 flex justify-end">
+        <label htmlFor="sort-method" className="font-medium">
+          Sort By
+          <select
+            id="sort-method"
+            name="sort-method"
+            className={`${styles.select} ml-3`}
+            onChange={handleChange}
+            defaultValue={sortMethod}
+          >
+            <option value={'best-selling'}>Best Selling</option>
+            <option value={'name-ascending'}>Alphabetically, A-Z</option>
+            <option value={'name-descending'}>Alphabetically, Z-A</option>
+            <option value={'price-ascending'}>Price, low to high</option>
+            <option value={'price-descending'}>Price, high to low</option>
+          </select>
+        </label>
+      </section>
+
       <section
-        className="xs:grid-cols-2 grid grid-cols-1 gap-5 sm:grid-cols-3 md:grid-cols-4
+        className="grid grid-cols-1 gap-5 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4
           lg:grid-cols-5"
       >
         {sortedProducts.map((product) => (
