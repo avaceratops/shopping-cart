5cc66c364084b7ca698db10a081722a9b9315c76
ava <148492613+avaceratops@users.noreply.github.com>
2024-02-21 15:31:24 +0000

    Change Home headings to match site heading
    Added leading-none and tracking-tight to h2 elements.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <article className="flex justify-center sm:px-12 md:px-24">
       <div className="flex max-w-5xl flex-col items-center">
-        <h2 className="mb-6 text-2xl font-bold">Coming Soon</h2>
+        <h2 className="mb-6 text-2xl font-bold leading-none tracking-tight">Coming Soon</h2>
         <section className="flex flex-col gap-5 md:gap-10">
           {promotionalImages.map((image, index) => (
             <img key={index} src={image} alt="" className="rounded-lg lg:rounded-2xl" />
@@ -23,7 +23,7 @@ export default function Home() {
 
         <div className="mb-10 mt-12 w-full border-b"></div>
 
-        <h2 className="mb-6 text-2xl font-bold">Products We Stock</h2>
+        <h2 className="mb-6 text-2xl font-bold leading-none tracking-tight">Products We Stock</h2>
         <section className="mb-10 grid max-w-5xl grid-cols-1 items-center gap-8 xs:grid-cols-2">
           {logos.map((image, index) => (
             <img key={index} src={image} alt="" />
