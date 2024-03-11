551f3efb0171a00df9189ae4c62ccd836049bc6d
ava <148492613+avaceratops@users.noreply.github.com>
2024-03-11 15:20:05 +0000

    Change Collapsible button font-weight to semibold
    This now matches the other semibold text on the Product page.

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -10,7 +10,7 @@ export default function Collapsible({ defaultOpen = false, buttonText, children 
             className={`flex w-full items-center justify-between rounded focus-visible:outline-none
             focus-visible:ring focus-visible:ring-indigo-500/50 ${open ? 'text-indigo-600' : ''}`}
           >
-            <span className="font-medium">{buttonText}</span>
+            <span className="font-semibold">{buttonText}</span>
             <span className="sr-only">{`${open ? 'Close' : 'Open'} panel`}</span>
             <div className="h-5 w-5">{open ? <MinusIcon /> : <PlusIcon />}</div>
           </Disclosure.Button>
